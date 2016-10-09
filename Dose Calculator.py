4b1204ca106c44e50a6e527e15d777982f87af8d
Yufan Xu <xu223@wisc.edu>
2016-10-09 18:12:22 -0500

    PIII Dose Calulator

---
+++ Dose Calculator.py
@@ -0,0 +1,7 @@
+__author__ = 'Yufan'
+import sys
+import numpy
+
+    charge = input('Total charge in coulomb: ')
+    print 'Total Number of Ions= '+str(-charge/1.602e-19)
+    print 'Dope= '+str(-charge/1.602e-19/18)+' cm^-2'
